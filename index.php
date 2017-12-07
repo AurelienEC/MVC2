d263279ecc855042d3bd95671b08fca4228c3d0c
Enireves <enireves.carre@gmail.com>
2017-12-07 20:30:40 +0100

    Ajouter postView.php et post.php

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
-session_start();
+// session_start();
 
+require('model.php');
 
-// Connexion à la base de données OC_blog_decembre_1.1
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=OC_blog_decembre_1.1;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch(Exception $e)
-{
-  die('Erreur : '.$e->getMessage());
-}
-
-
-
-// Récupération des 10 derniers messages
-$reponse = $bdd->query('SELECT id, titre, contenu, date_creation FROM billets ORDER BY ID DESC LIMIT 0, 10');
+$req = getPost();
 
 require('indexView.php');
+
 
 ?>
 
