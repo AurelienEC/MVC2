1d49595435f44a293d7aaf698eb49f783cc207c1
Enireves <enireves.carre@gmail.com>
2017-12-07 20:11:40 +0100

    Factorisation de model.php

--- model.php
+++ model.php
@@ -1,1 +1,46 @@
-model.php
+<?php
+function getPost()
+{
+	$db = dbConnect();
+
+	$req = $db->query('SELECT id, titre, contenu, date_creation FROM billets ORDER BY ID DESC LIMIT 0, 10');
+
+	return $req;
+}
+
+
+function getPost($postId)
+{
+    $db = dbConnect();
+
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(created_date, \'%d/%m/%Y à %Hh%imin%ss\') AS created_date_fr FROM posts WHERE id = ?');
+    $req->execute(array($postId));
+    $post = $req->fetch();
+
+    return $post;
+}
+
+
+function getComments($postId)
+{
+    $db = dbConnect();
+
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+    $comments->execute(array($postId));
+
+    return $comments;
+}
+
+
+function dbConnect()
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=OC_blog_decembre_1.1;charset=utf8', 'root', 'root');
+        return $db;
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+}
