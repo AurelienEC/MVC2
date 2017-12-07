f161b67eba767e0395f62cdff2f80d707b615a03
Enireves <enireves.carre@gmail.com>
2017-12-07 19:19:21 +0100

    Insertion de la bdd dans index.php

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 session_start();
 
 
-// Connexion à la BDD
+// Connexion à la base de données OC_blog_decembre_1.1
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=OC_blog_decembre_1.1;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+  die('Erreur : '.$e->getMessage());
+}
 
-include('bdd.php');
 
 
 // Récupération des 10 derniers messages
